2abb032a0a220c4d34eb92afb2e5a36ad5e1e740
Macmade <macmade@xs-labs.com>
2014-08-19 19:41:36 +0200

    Basic documentation...

---
+++ Documentation/Pages/Home.inc.php
@@ -0,0 +1,13 @@
+<div class="xsdoc-file">
+    <div class="xsdoc-file-content">
+        <h2>Objective-C Uniform Type Identifiers</h2>
+        <h3>About</h3>
+        <p>
+            ...
+        </p>
+        <h3>License</h3>
+        <p>
+            The project is released under the terms of the Boost Software License - Version 1.0.
+        </p>
+    </div>
+</div>
