33c97cca218f61bf593ef066466a52d957dbde69
macmade <macmade@xs-labs.com>
2015-06-12 13:36:52 +0200

    Unit tests...

--- UTI Tests/UTITest.m
+++ UTI Tests/UTITest.m
@@ -28,6 +28,8 @@
  * @abstract    Test class for UTI
  */
 
+@import UTI;
+
 @interface UTITest: XCTestCase
 {}
 
@@ -43,6 +45,222 @@
 - ( void )tearDown
 {
     [ super tearDown ];
+}
+
+- ( void )testUTIWithString
+{
+    UTI * uti;
+    
+    uti = [ UTI UTIWithString: @"public.objective-c-source" ];
+    
+    XCTAssertNotNil( uti );
+    XCTAssertEqualObjects( uti.identifier, @"public.objective-c-source" );
+}
+
+- ( void )testUTIWithCFString
+{
+    UTI       * uti;
+    CFStringRef s;
+    
+    s   = CFStringCreateWithCString( NULL, "public.objective-c-source", kCFStringEncodingASCII );
+    uti = [ UTI UTIWithCFString: s ];
+    
+    CFRelease( s );
+    
+    XCTAssertNotNil( uti );
+    XCTAssertEqualObjects( uti.identifier, @"public.objective-c-source" );
+}
+
+- ( void )testUTIWithFileExtension
+{
+    
+}
+
+- ( void )testUTIWithFileExtensionConformingTo
+{
+    
+}
+
+- ( void )testUTIWithMIMEType
+{
+    
+}
+
+- ( void )testUTIWithMIMETypeConformingTo
+{
+    
+}
+
+- ( void )testUTIWithNSPboardType
+{
+    
+}
+
+- ( void )testUTIWithNSPboardTypeConformingTo
+{
+    
+}
+
+- ( void )testUTIWithOSType
+{
+    
+}
+
+- ( void )testUTIWithOSTypeConformingTo
+{
+    
+}
+
+- ( void )testUTIWithTagTagClassConformingTo
+{
+    
+}
+
+- ( void )testAbstractTypes
+{
+    NSArray * types;
+    
+    types = [ UTI abstractTypes ];
+    
+    XCTAssertGreaterThan( types.count, ( NSUInteger )0 );
+    
+    for( UTI * uti in types )
+    {
+        XCTAssertTrue( [ uti isKindOfClass: [ UTI class ] ] );
+    }
+}
+
+- ( void )testConcreteTypes
+{
+    NSArray * types;
+    
+    types = [ UTI concreteTypes ];
+    
+    XCTAssertGreaterThan( types.count, ( NSUInteger )0 );
+    
+    for( UTI * uti in types )
+    {
+        XCTAssertTrue( [ uti isKindOfClass: [ UTI class ] ] );
+    }
+}
+
+- ( void )testTextTypes
+{
+    NSArray * types;
+    
+    types = [ UTI textTypes ];
+    
+    XCTAssertGreaterThan( types.count, ( NSUInteger )0 );
+    
+    for( UTI * uti in types )
+    {
+        XCTAssertTrue( [ uti isKindOfClass: [ UTI class ] ] );
+        XCTAssertTrue( [ uti conformsToUTI: [ UTI textTypeUTI ] ] );
+    }
+}
+
+- ( void )testCompositeContentTypes
+{
+    NSArray * types;
+    
+    types = [ UTI compositeContentTypes ];
+    
+    XCTAssertGreaterThan( types.count, ( NSUInteger )0 );
+    
+    for( UTI * uti in types )
+    {
+        XCTAssertTrue( [ uti isKindOfClass: [ UTI class ] ] );
+        XCTAssertTrue( [ uti conformsToUTI: [ UTI compositeContentTypeUTI ] ] );
+    }
+}
+
+- ( void )testImageContentTypes
+{
+    NSArray * types;
+    
+    types = [ UTI imageContentTypes ];
+    
+    XCTAssertGreaterThan( types.count, ( NSUInteger )0 );
+    
+    for( UTI * uti in types )
+    {
+        XCTAssertTrue( [ uti isKindOfClass: [ UTI class ] ] );
+        XCTAssertTrue( [ uti conformsToUTI: [ UTI imageTypeUTI ] ] );
+    }
+}
+
+- ( void )testAudioVisualContentTypes
+{
+    NSArray * types;
+    
+    types = [ UTI audioVisualContentTypes ];
+    
+    XCTAssertGreaterThan( types.count, ( NSUInteger )0 );
+    
+    for( UTI * uti in types )
+    {
+        XCTAssertTrue( [ uti isKindOfClass: [ UTI class ] ] );
+        XCTAssertTrue( [ uti conformsToUTI: [ UTI audiovisualContentTypeUTI ] ] );
+    }
+}
+
+- ( void )testDirectoryTypes
+{
+    NSArray * types;
+    
+    types = [ UTI directoryTypes ];
+    
+    XCTAssertGreaterThan( types.count, ( NSUInteger )0 );
+    
+    for( UTI * uti in types )
+    {
+        XCTAssertTrue( [ uti isKindOfClass: [ UTI class ] ] );
+        XCTAssertTrue( [ uti conformsToUTI: [ UTI directoryTypeUTI ] ] );
+    }
+}
+
+- ( void )testApplicationTypes
+{
+    NSArray * types;
+    
+    types = [ UTI applicationTypes ];
+    
+    XCTAssertGreaterThan( types.count, ( NSUInteger )0 );
+    
+    for( UTI * uti in types )
+    {
+        XCTAssertTrue( [ uti isKindOfClass: [ UTI class ] ] );
+        XCTAssertTrue( [ uti conformsToUTI: [ UTI applicationTypeUTI ] ] );
+    }
+}
+
+- ( void )testContactTypes
+{
+    NSArray * types;
+    
+    types = [ UTI contactTypes ];
+    
+    XCTAssertGreaterThan( types.count, ( NSUInteger )0 );
+    
+    for( UTI * uti in types )
+    {
+        XCTAssertTrue( [ uti isKindOfClass: [ UTI class ] ] );
+        XCTAssertTrue( [ uti conformsToUTI: [ UTI contactTypeUTI ] ] );
+    }
+}
+
+- ( void )testMiscellaneousTypes
+{
+    NSArray * types;
+    
+    types = [ UTI miscellaneousTypes ];
+    
+    XCTAssertGreaterThan( types.count, ( NSUInteger )0 );
+    
+    for( UTI * uti in types )
+    {
+        XCTAssertTrue( [ uti isKindOfClass: [ UTI class ] ] );
+    }
 }
 
 @end
