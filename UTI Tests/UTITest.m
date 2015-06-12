f00c2b6f13bc90e29c2654de71eda6d045213bb0
macmade <macmade@xs-labs.com>
2015-06-12 14:14:11 +0200

    Unit tests.

--- UTI Tests/UTITest.m
+++ UTI Tests/UTITest.m
@@ -263,4 +263,344 @@
     }
 }
 
+- ( void )testItemTypeUTI
+{
+    XCTAssertNotNil( [ UTI itemTypeUTI ] );
+}
+
+- ( void )testContentTypeUTI
+{
+    XCTAssertNotNil( [ UTI contentTypeUTI ] );
+}
+
+- ( void )testCompositeContentTypeUTI
+{
+    XCTAssertNotNil( [ UTI compositeContentTypeUTI ] );
+}
+
+- ( void )testApplicationTypeUTI
+{
+    XCTAssertNotNil( [ UTI applicationTypeUTI ] );
+}
+
+- ( void )testMessageTypeUTI
+{
+    XCTAssertNotNil( [ UTI messageTypeUTI ] );
+}
+
+- ( void )testContactTypeUTI
+{
+    XCTAssertNotNil( [ UTI contactTypeUTI ] );
+}
+
+- ( void )testArchiveTypeUTI
+{
+    XCTAssertNotNil( [ UTI archiveTypeUTI ] );
+}
+
+- ( void )testDiskImageTypeUTI
+{
+    XCTAssertNotNil( [ UTI diskImageTypeUTI ] );
+}
+
+- ( void )testDataTypeUTI
+{
+    XCTAssertNotNil( [ UTI dataTypeUTI ] );
+}
+
+- ( void )testDirectoryTypeUTI
+{
+    XCTAssertNotNil( [ UTI directoryTypeUTI ] );
+}
+
+- ( void )testResolvableTypeUTI
+{
+    XCTAssertNotNil( [ UTI resolvableTypeUTI ] );
+}
+
+- ( void )testSymLinkTypeUTI
+{
+    XCTAssertNotNil( [ UTI symLinkTypeUTI ] );
+}
+
+- ( void )testMountPointTypeUTI
+{
+    XCTAssertNotNil( [ UTI mountPointTypeUTI ] );
+}
+
+- ( void )testAliasFileTypeUTI
+{
+    XCTAssertNotNil( [ UTI aliasFileTypeUTI ] );
+}
+
+- ( void )testAliasRecordTypeUTI
+{
+    XCTAssertNotNil( [ UTI aliasRecordTypeUTI ] );
+}
+
+- ( void )testURLTypeUTI
+{
+    XCTAssertNotNil( [ UTI URLTypeUTI ] );
+}
+
+- ( void )testFileURLTypeUTI
+{
+    XCTAssertNotNil( [ UTI fileURLTypeUTI ] );
+}
+
+- ( void )testTextTypeUTI
+{
+    XCTAssertNotNil( [ UTI textTypeUTI ] );
+}
+
+- ( void )testPlainTextTypeUTI
+{
+    XCTAssertNotNil( [ UTI plainTextTypeUTI ] );
+}
+
+- ( void )testUTF8PlainTextTypeUTI
+{
+    XCTAssertNotNil( [ UTI UTF8PlainTextTypeUTI ] );
+}
+
+- ( void )testUTF16ExternalPlainTextTypeUTI
+{
+    XCTAssertNotNil( [ UTI UTF16ExternalPlainTextTypeUTI ] );
+}
+
+- ( void )testUTF16PlainTextTypeUTI
+{
+    XCTAssertNotNil( [ UTI UTF16PlainTextTypeUTI ] );
+}
+
+- ( void )testRTFTypeUTI
+{
+    XCTAssertNotNil( [ UTI RTFTypeUTI ] );
+}
+
+- ( void )testHTMLTypeUTI
+{
+    XCTAssertNotNil( [ UTI HTMLTypeUTI ] );
+}
+
+- ( void )testXMLTypeUTI
+{
+    XCTAssertNotNil( [ UTI XMLTypeUTI ] );
+}
+
+- ( void )testSourceCodeTypeUTI
+{
+    XCTAssertNotNil( [ UTI sourceCodeTypeUTI ] );
+}
+
+- ( void )testCSourceTypeUTI
+{
+    XCTAssertNotNil( [ UTI CSourceTypeUTI ] );
+}
+
+- ( void )testObjectiveCSourceTypeUTI
+{
+    XCTAssertNotNil( [ UTI objectiveCSourceTypeUTI ] );
+}
+
+- ( void )testCPlusPlusSourceTypeUTI
+{
+    XCTAssertNotNil( [ UTI CPlusPlusSourceTypeUTI ] );
+}
+
+- ( void )testObjectiveCPlusPlusSourceTypeUTI
+{
+    XCTAssertNotNil( [ UTI objectiveCPlusPlusSourceTypeUTI ] );
+}
+
+- ( void )testCHeaderTypeUTI
+{
+    XCTAssertNotNil( [ UTI CHeaderTypeUTI ] );
+}
+
+- ( void )testCPlusPlusHeaderTypeUTI
+{
+    XCTAssertNotNil( [ UTI CPlusPlusHeaderTypeUTI ] );
+}
+
+- ( void )testJavaSourceTypeUTI
+{
+    XCTAssertNotNil( [ UTI javaSourceTypeUTI ] );
+}
+
+- ( void )testPDFTypeUTI
+{
+    XCTAssertNotNil( [ UTI PDFTypeUTI ] );
+}
+
+- ( void )testRTFDTypeUTI
+{
+    XCTAssertNotNil( [ UTI RTFDTypeUTI ] );
+}
+
+- ( void )testFlatRTFDTypeUTI
+{
+    XCTAssertNotNil( [ UTI flatRTFDTypeUTI ] );
+}
+
+- ( void )testTXNTextAndMultimediaDataTypeUTI
+{
+    XCTAssertNotNil( [ UTI TXNTextAndMultimediaDataTypeUTI ] );
+}
+
+- ( void )testWebArchiveTypeUTI
+{
+    XCTAssertNotNil( [ UTI webArchiveTypeUTI ] );
+}
+
+- ( void )testImageTypeUTI
+{
+    XCTAssertNotNil( [ UTI imageTypeUTI ] );
+}
+
+- ( void )testJPEGTypeUTI
+{
+    XCTAssertNotNil( [ UTI JPEGTypeUTI ] );
+}
+
+- ( void )testJPEG2000TypeUTI
+{
+    XCTAssertNotNil( [ UTI JPEG2000TypeUTI ] );
+}
+
+- ( void )testTIFFTypeUTI
+{
+    XCTAssertNotNil( [ UTI TIFFTypeUTI ] );
+}
+
+- ( void )testPICTTypeUTI
+{
+    XCTAssertNotNil( [ UTI PICTTypeUTI ] );
+}
+
+- ( void )testGIFTypeUTI
+{
+    XCTAssertNotNil( [ UTI GIFTypeUTI ] );
+}
+
+- ( void )testPNGTypeUTI
+{
+    XCTAssertNotNil( [ UTI PNGTypeUTI ] );
+}
+
+- ( void )testQuickTimeImageTypeUTI
+{
+    XCTAssertNotNil( [ UTI quickTimeImageTypeUTI ] );
+}
+
+- ( void )testAppleICNSTypeUTI
+{
+    XCTAssertNotNil( [ UTI appleICNSTypeUTI ] );
+}
+
+- ( void )testBMPTypeUTI
+{
+    XCTAssertNotNil( [ UTI BMPTypeUTI ] );
+}
+
+- ( void )testICOTypeUTI
+{
+    XCTAssertNotNil( [ UTI ICOTypeUTI ] );
+}
+
+- ( void )testAudiovisualContentTypeUTI
+{
+    XCTAssertNotNil( [ UTI audiovisualContentTypeUTI ] );
+}
+
+- ( void )testMovieTypeUTI
+{
+    XCTAssertNotNil( [ UTI movieTypeUTI ] );
+}
+
+- ( void )testVideoTypeUTI
+{
+    XCTAssertNotNil( [ UTI videoTypeUTI ] );
+}
+
+- ( void )testAudioTypeUTI
+{
+    XCTAssertNotNil( [ UTI audioTypeUTI ] );
+}
+
+- ( void )testQuickTimeMovieTypeUTI
+{
+    XCTAssertNotNil( [ UTI quickTimeMovieTypeUTI ] );
+}
+
+- ( void )testMPEGTypeUTI
+{
+    XCTAssertNotNil( [ UTI MPEGTypeUTI ] );
+}
+
+- ( void )testMPEG4TypeUTI
+{
+    XCTAssertNotNil( [ UTI MPEG4TypeUTI ] );
+}
+
+- ( void )testMP3TypeUTI
+{
+    XCTAssertNotNil( [ UTI MP3TypeUTI ] );
+}
+
+- ( void )testMPEG4AudioTypeUTI
+{
+    XCTAssertNotNil( [ UTI MPEG4AudioTypeUTI ] );
+}
+
+- ( void )testAppleProtectedMPEG4AudioTypeUTI
+{
+    XCTAssertNotNil( [ UTI appleProtectedMPEG4AudioTypeUTI ] );
+}
+
+- ( void )testFolderTypeUTI
+{
+    XCTAssertNotNil( [ UTI folderTypeUTI ] );
+}
+
+- ( void )testVolumeTypeUTI
+{
+    XCTAssertNotNil( [ UTI volumeTypeUTI ] );
+}
+
+- ( void )testPackageTypeUTI
+{
+    XCTAssertNotNil( [ UTI packageTypeUTI ] );
+}
+
+- ( void )testBundleTypeUTI
+{
+    XCTAssertNotNil( [ UTI bundleTypeUTI ] );
+}
+
+- ( void )testFrameworkTypeUTI
+{
+    XCTAssertNotNil( [ UTI frameworkTypeUTI ] );
+}
+
+- ( void )testApplicationBundleTypeUTI
+{
+    XCTAssertNotNil( [ UTI applicationBundleTypeUTI ] );
+}
+
+- ( void )testApplicationFileTypeUTI
+{
+    XCTAssertNotNil( [ UTI applicationFileTypeUTI ] );
+}
+
+- ( void )testVCardTypeUTI
+{
+    XCTAssertNotNil( [ UTI vCardTypeUTI ] );
+}
+
+- ( void )testInkTextTypeUTI
+{
+    XCTAssertNotNil( [ UTI inkTextTypeUTI ] );
+}
+
 @end
